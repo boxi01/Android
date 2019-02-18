1ece300df970c923045c0921c8752f03992cb28f
boxi01 <45149847+boxi01@users.noreply.github.com>
2019-02-19 00:20:33 +0100

    Images display correctly, link URL works

--- app/src/main/java/com/example/myapplication/favouriteQuotes.java
+++ app/src/main/java/com/example/myapplication/favouriteQuotes.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,13 +21,15 @@ public class favouriteQuotes extends AppCompatActivity {
 
             @Override
             public void onClick(View v){
-                refreshQuote();
+                infoAboutAuthor();
             }
         });
     }
 
-    public void refreshQuote(){
-        Intent intent = new Intent(this, authorInfo.class);
+    public void infoAboutAuthor(){
+        String website = "https://pl.wikipedia.org/wiki/Mahatma_Gandhi";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(website));
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/openAbout.java
+++ app/src/main/java/com/example/myapplication/openAbout.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class openAbout extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class openAbout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_about);
+
+        ImageView myPhoto = (ImageView) findViewById(R.id.myPhoto);
+        ImageView myPhoto2 = (ImageView) findViewById(R.id.myPhoto2);
+
+        int imageResource = getResources().getIdentifier("@drawable/photo", null, this.getPackageName());
+        myPhoto.setImageResource(imageResource);
+        myPhoto2.setImageResource(imageResource);
     }
 }
