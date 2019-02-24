3da9f081ac859896889d01c2270278e49d04cee3
Pawel Maslowski <pawel.maslowski123@gmail.com>
2019-02-24 17:34:16 +0100

    Added menu for QuotationActivity

--- app/src/main/java/com/example/myapplication/QuotationActivity.java
+++ app/src/main/java/com/example/myapplication/QuotationActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -30,7 +33,30 @@ public class QuotationActivity extends AppCompatActivity {
     }
 
     public void refreshQuote(){
-        Intent intent = new Intent(this, RefreshQuote.class);
-        startActivity(intent);
+        TextView sampleQ = findViewById(R.id.getQuote);
+        sampleQ.setText(R.string.refreshQuote);
+        TextView sampleAuthor = findViewById(R.id.sampleAuthor);
+        sampleAuthor.setText("~ "+getString(R.string.author));
+        sampleAuthor.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menus,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.getNewQ:
+                refreshQuote();
+                return true;
+            case R.id.addToFav:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
